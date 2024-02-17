7273f69161e4deb9662695bbada4f46776ae0e85
David Nash <hisownspace@gmail.com>
2024-02-16 20:32:52 -0500

    add replace method in config class for sqlalchemy compatibilitys

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
