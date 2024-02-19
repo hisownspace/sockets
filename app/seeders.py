bb50b109beed930273b97f71db8bdc2f53dd6fba
David Nash <hisownspace@gmail.com>
2024-02-19 14:26:49 -0500

    add more seeder messages

--- app/seeders.py
+++ app/seeders.py
@@ -1,7 +1,20 @@
 import os
+from datetime import datetime
+from random import random
+from app.models.messages import Message
 from sqlalchemy.sql import text
 from flask.cli import AppGroup
 from .models import db, User, Room
+
+
+def random_date_2024():
+    start_date = datetime(2024, 1, 1)
+    end_date = datetime.now()
+    time_elapsed = end_date - start_date
+    random_time = time_elapsed * random()
+    random_date = start_date + random_time
+    return random_date
+
 
 environment = os.environ.get("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
@@ -10,13 +23,16 @@ seed_commands = AppGroup("seed")
 
 
 def seed_rooms():
-    room_1 = Room(name="Serenity")
-    room_2 = Room(name="Miranda")
-    room_3 = Room(name="Persephone")
-    db.session.add(room_1)
-    db.session.add(room_2)
-    db.session.add(room_3)
+    rooms = {
+        "serenity": Room(name="Serenity"),
+        "miranda": Room(name="Miranda"),
+        "persephone": Room(name="Persephone"),
+    }
+    for room in rooms.values():
+        db.session.add(room)
     db.session.commit()
+
+    return rooms
 
 
 def undo_rooms():
@@ -30,26 +46,22 @@ def undo_rooms():
 
 
 def seed_users():
-    user_1 = User(username="Mal", password="password", theme="#7B6859")
-    user_2 = User(username="Zoe", password="password", theme="#ACACA2")
-    user_3 = User(username="Wash", password="password", theme="#C8A680")
-    user_4 = User(username="Inara", password="password", theme="#457E89")
-    user_5 = User(username="Jayne", password="password", theme="#E76D2D")
-    user_6 = User(username="River", password="password", theme="#969249")
-    user_7 = User(username="Kaylee", password="password", theme="#A7AF9D")
-    user_8 = User(username="Simon", password="password", theme="#6A9DA1")
-    user_9 = User(username="Book", password="password", theme="#7B6859")
+    users = {
+        "mal": User(username="Mal", password="password", theme="#7B6859"),
+        "zoe": User(username="Zoe", password="password", theme="#ACACA2"),
+        "wash": User(username="Wash", password="password", theme="#C8A680"),
+        "inara": User(username="Inara", password="password", theme="#457E89"),
+        "jayne": User(username="Jayne", password="password", theme="#E76D2D"),
+        "river": User(username="River", password="password", theme="#969249"),
+        "kaylee": User(username="Kaylee", password="password", theme="#A7AF9D"),
+        "simon": User(username="Simon", password="password", theme="#6A9DA1"),
+        "book": User(username="Book", password="password", theme="#7B6859"),
+    }
 
-    db.session.add(user_1)
-    db.session.add(user_2)
-    db.session.add(user_3)
-    db.session.add(user_4)
-    db.session.add(user_5)
-    db.session.add(user_6)
-    db.session.add(user_7)
-    db.session.add(user_8)
-    db.session.add(user_9)
+    for user in users.values():
+        db.session.add(user)
     db.session.commit()
+    return users
 
 
 def undo_users():
@@ -60,6 +72,167 @@ def undo_users():
     else:
         db.session.execute(text("DELETE FROM users;"))
     db.session.commit()
+
+
+def seed_messages(users, rooms):
+    message_1 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_2 = Message(
+        content="We have done the impossible, and that makes us mighty.",
+        room=rooms["persephone"],
+        user=users["mal"],
+    )
+    message_3 = Message(
+        content="I believe that woman's plannin' to shoot me again.",
+        room=rooms["serenity"],
+        user=users["mal"],
+    )
+    message_4 = Message(
+        content="I'm very sorry if she tipped off anyone about your cunningly concealed herd of cows.",
+        room=rooms["persephone"],
+        user=users["simon"],
+    )
+    message_5 = Message(
+        content="This place gives me an uncomfortableness...",
+        room=rooms["miranda"],
+        user=users["jayne"],
+    )
+    message_6 = Message(
+        content="It's been a big day, what with the abduction and all.",
+        room=rooms["serenity"],
+        user=users["simon"],
+    )
+    message_7 = Message(
+        content="It's good to be home",
+        room=rooms["serenity"],
+        user=users["book"],
+    )
+    message_8 = Message(
+        content="Someone ever tries to kill you, you try to kill 'em right back!",
+        room=rooms["miranda"],
+        user=users["mal"],
+    )
+    message_9 = Message(
+        content="Every planet has its own weird customs. About a year before we met, I spent six weeks on a moon where the principal form of recreation was juggling geese. My hand to God. Baby geese. Goslings. They were juggled.",
+        room=rooms["persephone"],
+        user=users["wash"],
+    )
+    message_10 = Message(
+        content="The hero of Canton, the man they call 'me'.",
+        room=rooms["miranda"],
+        user=users["jayne"],
+    )
+    message_11 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["persephone"],
+        user=users["kaylee"],
+    )
+    message_12 = Message(
+        content="Sometimes a thing gets broke, can't be fixed.",
+        room=rooms["serenity"],
+        user=users["kaylee"],
+    )
+    message_13 = Message(
+        content="Two by two, hands of blue... two by two, hands of blue...",
+        room=rooms["miranda"],
+        user=users["river"],
+    )
+    message_14 = Message(
+        content="They don't like it when you shoot at 'em. I worked that out myself.",
+        room=rooms["miranda"],
+        user=users["mal"],
+    )
+    message_15 = Message(
+        content="Nothing worse than a monster who thinks he's right with God.",
+        room=rooms["miranda"],
+        user=users["mal"],
+    )
+    message_16 = Message(
+        content="It's just an object. It doesn't mean what you think.",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_17 = Message(
+        content="You are such a boob.",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_18 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_19 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_20 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_21 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_22 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_23 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_24 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+    message_25 = Message(
+        content="No power in the 'verse can stop me!",
+        room=rooms["serenity"],
+        user=users["river"],
+    )
+
+    messages = [
+        message_1,
+        message_2,
+        message_3,
+        message_4,
+        message_5,
+        message_6,
+        message_7,
+        message_8,
+        message_9,
+        message_10,
+        message_11,
+        message_12,
+        message_13,
+        message_14,
+        message_15,
+        message_16,
+        message_17,
+        message_18,
+        message_19,
+        message_20,
+        message_21,
+        message_22,
+        message_23,
+        message_24,
+        message_25,
+    ]
+
+    for message in messages:
+        random_date = random_date_2024()
+        message.created_at = random_date
+        message.created_at = random_date
 
 
 def undo_messages():
@@ -113,8 +286,9 @@ def seed_all():
         undo_rooms()
         undo_users()
         undo_messages()
-    seed_users()
-    seed_rooms()
+    users = seed_users()
+    rooms = seed_rooms()
+    seed_messages(users, rooms)
 
 
 @seed_commands.command("undo")
