39f7c73730904238bc8d25e8a6cef3438b4b150f
David Nash <hisownspace@gmail.com>
2024-02-20 18:43:22 -0500

    change URL in fe socket initialization

--- react-app/src/context/socket.jsx
+++ react-app/src/context/socket.jsx
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV == "production"
-    ? "wss://websockets-testing.onrender.com"
+    ? "wss://serenity-chat.onrender.com"
     : "localhost:5000";
 
 console.log(URL);
